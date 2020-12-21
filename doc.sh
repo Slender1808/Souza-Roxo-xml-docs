7595a21f0a917d6471dc4627d11f6c66f9cc7ec9
Jadson G. Matos <50336793+Slender1808@users.noreply.github.com>
2020-12-21 16:17:05 -0300

    Update doc.sh

--- doc.sh
+++ doc.sh
@@ -3,4 +3,4 @@ wget --no-check-certificate --quiet \
   --timeout=0 \
   --header 'Content-Type: application/json' \
   --body-data '{"query":"mutation insertXml($xml: xml!,$user: uuid!) {\r\n  insert_XML(objects: {xml: $xml, user: $user}) {\r\n    affected_rows\r\n  }\r\n}","variables":{"xml":"<xml>abc</xml>","user":"86ab5b35-750e-459d-93a4-3a262478c117"}}' \
-   'https://skw.souzaroxo.com.br/v1/graphql'
+   'https://skw.souzaroxo.com.br:442/v1/graphql'
